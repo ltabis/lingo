81c9c1f52429a54cb29bddf6e6bf93ab561b3ddc
lucas.tabis <lucas.tabis@localhost.localdomain>
2018-05-21 15:18:15 +0200

    added loose conditions

--- src/game_loop.c
+++ src/game_loop.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../include/proto.h"
 
 char *choose_random_word(char **av)
@@ -36,11 +37,17 @@ int start_game_loop(char *word)
 {
 	char *line = NULL;
 	size_t len = 0;
+	const unsigned int word_len = strlen(word);
+	unsigned int i = 2;
 
 	display_first_lines(word);
 	printf("Round %i\n>", 1);
-	for (unsigned int i = 2;
+	for (; i < word_len &&
 	getline(&line, &len, stdin) != -1 && line; i++) {
-		printf("Round %i\n>", i);
+		printf("Round %i>\n", i);
 	}
+	if (word_len == i)
+		printf("You lost!\n");
+	else
+		printf("You won!\n");
 }
